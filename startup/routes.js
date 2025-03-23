b9abe7b761dfd9cafb484b21c67c15d6daa5f08b
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-23 08:36:26 +0530

    add user path

--- startup/routes.js
+++ startup/routes.js
@@ -1,7 +1,11 @@
-const router = require('../routes/products')
+const auth = require('../routes/auth')
+const products = require('../routes/products')
+const users = require('../routes/user')
 const express = require('express')
 
 module.exports = function (app){
-    app.use(express.json());
-    app.use('/api/products', router);
+    app.use(express.json())
+    app.use('/api/products', products)
+    app.use('/api/users', users)
+    app.use('/api/auth', auth)
 }
