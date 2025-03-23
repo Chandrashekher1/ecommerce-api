76ff1e7af7adb72fca7b1aa36b2e49d7a80379be
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-23 08:35:27 +0530

    add: user path

---
+++ routes/user.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken')
+const config = require('config')
+const bcrypt = require('bcrypt')
+const {User,validate} = require('../models/users')
+const express = require('express')
+const router = express.Router()
+
+
+router.post('/',async (req,res) => {
+    const {FirstName, LastName,Email, password} = req.body
+    const {error} = validate(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+    let user = await User.findOne({Email:Email})
+
+    if(user) {
+        return res.status(400).send("User is already exist")
+    }
+    user = new User({
+        FirstName:FirstName,
+        LastName: LastName,
+        Email : Email,
+        password : password
+    })
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(password,salt)
+    user = await user.save()
+    // if register then direct login in
+    const token = jwt.sign({_id: user._id},process.env.shop_jwtPrivateKey)
+    
+    res.header('x-auth-token',token).send(user)
+})
+
+module.exports = router
