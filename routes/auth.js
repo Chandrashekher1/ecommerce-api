329523ff9b28a199789851e652241deee18d5575
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-03-23 08:34:21 +0530

    add : auth(login) path

---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+const config = require('config')
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const Joi = require('joi')
+const {User} = require('../models/users')
+const express = require('express')
+const router = express.Router()
+
+router.post('/', async (req,res) => {
+    const {Email, password} = req.body
+    const {error} = validate(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+
+    let user = await User.findOne({Email:Email})
+    if(!user) return res.status(400).send("User is not registered")
+
+    const validPassword = await bcrypt.compare(password,user.password)
+    if(!validPassword) return res.status(400).send("Invalid Password")
+    const token = jwt.sign({_id: user._id}, config.get(process.env.shop_jwtPrivateKey))
+    res.send(token)
+})
+
+function validate(user){
+    const Schema = Joi.object({
+        Email: Joi.string().min(0).max(2000).required(),
+        password: Joi.number().required(),
+    })
+   return Schema.validate(user);
+}
+module.exports = router
